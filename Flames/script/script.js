fed73c6900b568fdc1802e2bd1b97e13545c1c2f
ManojKumarsgit <114407250+ManojKumarsgit@users.noreply.github.com>
2023-10-21 15:45:19 +0530

    Update script.js

--- Flames/script/script.js
+++ Flames/script/script.js
@@ -12,7 +12,8 @@ const mainpara = document.querySelector('.main-para');
 const movieName = document.querySelector('.movie-name')
 const resImg = document.getElementById('resImage');
 
-alert("hello from git");
+    alert(`Please.. Adjust your Phone Volume Based on your Environment!. "AutoPlay" audio is enabled in this site. You can Pause it under the Image`);
+
 
     let isError = false;
 
@@ -27,44 +28,46 @@ alert("hello from git");
       fqoute.textContent = `"FUN QUOTES"`
       chkBtn.innerHTML =""
       btn.style = "display:inline-block"
-      playHeader2.textContent = "Tap to Play"
+      playHeader2.textContent = "TAP TO PAUSE"
       resetTime();
     })
     
 
-    let isPlaying = false;
+    let isPlaying = true;
     let lastPlayedTime = 0;
     const audio = document.getElementById("myAudio");
 
     function resetTime() {
       audio.pause();
       lastPlayedTime = 0;
-      icon.classList.remove('fa-pause-circle')
-      icon.classList.add('fa-play-circle')
-      playBtn.style = "background-color: greenyellow !important;"
+      icon.classList.remove('fa-play-circle')
+      icon.classList.add('fa-pause-circle')
+      playBtn.style = "background-color: red !important;"
     }
 
-    let affSongs = ["Affect1"]
-    let loveSongs = ["love1","love2"]
-    let sisSongs = ["sister1","sister2"]
-    let marrSongs = ["marriage1","marriage2"]
+    let friSongs = ["friend1","friend2","friend3","friend4","friend5"]
+    let loveSongs = ["love1","love2","love3","love4","love5"]
+    let affSongs = ["affection1","affection2","affection3","affection4","affection5"]
+    let marrSongs = ["marriage1","marriage2","marriage3","marriage4","marriage5"]
     let enemSongs = ["enemy"]
-    let friSongs = ["friend2"]
+    let sisSongs = ["sister1","sister2","sister3","sister4","sister5"]
 
-    audio.src = affSongs[0];
+    // audio.src = affSongs[0];
     playBtn.addEventListener('click',() =>{
       if(!isPlaying){
         icon.classList.remove('fa-play-circle')
         icon.classList.add('fa-pause-circle')
         audio.currentTime = lastPlayedTime;
         audio.play();
-        playBtn.style = "background-color: red !important;"
+        playHeader2.textContent = "TAP TO PAUSE"
+        playBtn.style = "background-color: red;"
         isPlaying = true;
       } else {
         icon.classList.remove('fa-pause-circle')
         icon.classList.add('fa-play-circle')
         lastPlayedTime = audio.currentTime;
         audio.pause();
+        playHeader2.textContent = "TAP TO PLAY"
         console.log(lastPlayedTime);
         playBtn.style = "background-color: greenyellow !important;"
         isPlaying = false;
@@ -82,7 +85,7 @@ alert("hello from git");
     icon.classList.add('fa-play-circle')
     isPlaying = false;
     lastPlayedTime = 0;
-    playBtn.style = "background-color: red"
+    playBtn.style = "background-color: greenyellow !important;"
     playHeader2.textContent = "PLAY AGAIN"
   }
 
@@ -226,44 +229,43 @@ for (let i = 0; i < n1.length; i++) {
       hide.classList.remove('hide')
       hide.classList.add('show');
     }
-
-
+    
     switch (result) {
       case 'F':
         result = "FRIEND 🤝"
         // btn.innerHTML = "FRIEND👯‍♂️"
-        fqoute.innerHTML = "FRIENDS DON'T JUDGE, THEY LAUGH!😆"
+        fqoute.innerHTML = `"${friendQoute[randomNum()]}"`;
         friend();
         break;
       case 'L':
         result = "LOVE ❤"
         // btn.innerHTML = "LOVE❤"
-        fqoute.innerHTML = "YOU GOT LOVE, BUT MARRIAGE IS DOUBT😂🤣"
+        fqoute.innerHTML = `"${loveQoute[randomNum()]}"`;
         love();
         break;
       case 'A':
         // btn.innerHTML = "AFFECTION"
         result = "AFFECTION 💔"
-        fqoute.innerHTML = "ONE-SIDED LOVE IS LIKE A SELFIE WITH NO LIKES😶"
+        fqoute.innerHTML = `"${affectQoute[randomNum()]}"`;
         affection();
         break;
       case 'M':
         // btn.innerHTML = "MARRIAGE💑😍"
         result = "MARRIAGE 💑"
-        fqoute.innerHTML = "YOU GOT MARRIAGE, BUT WHERE IS LOVE😆🤔"
+        fqoute.innerHTML = `"${marriageQoute[randomNum()]}"`;
         marriage();
         break; 
       case 'E':
         // btn.innerHTML = "ENEMY"
         result = "ENEMY😡"
-        fqoute.innerHTML = "HEY ENEMY, WE'RE LIKE TOMATOES AND KETCHUP😆"
+        fqoute.innerHTML = `"${enemyQoute[randomNum()]}"`;
         enemy();
         break;
       case 'S':
         // btn.innerHTML = "SISTER👩‍👦"
         result = "SISTER 👩‍👦"
-        fqoute.innerHTML = "AHH! IT's HARD TO THINK LIKE SISTER😆"
-        sister()
+        fqoute.innerHTML = `"${sisterQoute[randomNum()]}"`;
+        sister();
         break;                 
       default:
         break;
@@ -272,6 +274,7 @@ for (let i = 0; i < n1.length; i++) {
     }
     chkBtn.innerHTML = result;
     btn.style = "display:none;";
+    audio.play();
       
 })
 
@@ -282,59 +285,123 @@ let marriageMov = ["Sillunu Oru Kaadhal (2006)","Thaandavam (2012)","Raja Rani (
 let enemyMov = ["Kodi (2016)","Devathaiyai Kanden (2005)","Thimiru (2006)","Vallavan (2006)","Padayappa (1999)"];
 let sisterMov = ["Sivappu Manjal Pachai (2019)","Thirupaachi (2005)","Namma Veettu Pillai (2019)","Velayudham (2011)","Vedi (2011)"];
 
+// Images
+
+let friendImages = ["friend1","friend2","friend3","friend4","friend5"];
+let loveImages = ["love1","love2","love3","love4","love5"];
+let affectImages = ["affect1","affect2","affect3","affect4","affect5"];
+let marriageImages = ["marriage1","marriage2","marriage3","marriage4","marriage5"];
+let enemyImages = ["enemy1","enemy2","enemy3","enemy4","enemy5"];
+let sisterImages = ["sister1","sister2","sister3","sister4","sister5"];
+
+// qoutes
+let friendQoute = [
+    "Friends don't Judge, They Laugh!",
+    "A Friend is Someone Who Knows you and Loves you just the same",
+    "Friendship is the only cement that will ever hold the world together",
+    "Friendship is like a wine, it gets better with age",
+    "Friends are the family you choose"
+];
+
+let loveQoute = [
+  "Love is like a roller coaster. It has its ups and downs, but it's always a thrilling ride.",
+  "Love is like a game. It has its winners and losers, but it's always fun to play.",
+  "Love is a language that everyone understands.",
+  "Love is when you can't imagine your future without them.",
+  "YOU GOT LOVE, BUT MARRIAGE IS DOUBT😂🤣"
+];
+
+let affectQoute = [
+  "Your smile warms my heart",
+  "With you, I've found my forever.",
+  "One-Sided Love is like a Selfie with no likes😶",
+  "I may be one-sided, but my love is 100% genuine.",
+  `I'm in the 'waiting for a reply' stage of my love story`
+];
+
+let marriageQoute = [
+  "You got Marriage but, Where is Love😂",
+  "Love is blind, but marriage is a real eye-opener",
+  "A successful marriage requires falling in love many times, always with the same person.",
+  "Marriage is finding that one special person to annoy😆 for a lifetime.",
+  "Marriage is an adventure, like going to war 😅"
+];
+
+let enemyQoute = [
+  "Keep your friends close and your enemies in a 'block' list.",
+  "Enemies are like unused apps; they take up space but serve no purpose.",
+  "I love my enemies so much; I keep them in my heart...shaped punching bag.",
+  `I'm not going to waste my time trying to understand you.`,
+  "I'm going to keep fighting until the very end.😉"
+];
+
+let sisterQoute = [
+  "Sisters are like built-in best friends.",
+  "A sister is a little bit of childhood that can never be lost.",
+  "Sisters are connected by the heart.",
+  "Ahh! it's Hard to Think like a Sister😆",
+  "Sisters are the people who know you best, and love you the most."
+];
+
+
+
+
+
+
 let random = 0;
 function friend() {
   mainpara.innerHTML = `IF YOU WANT TO FEEL WHAT THE ACTUAL <span class="side">FRIEND🤝</span> IS`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = friendMov[randomNumMov()];
-  audio.src = `./audio/${friSongs[0]}.mp3`;
-  resImg.src = "./images/friend.jpg"
+  movieName.textContent = friendMov[randomNum()];
+  audio.src = `./audio/Friend/${friSongs[randomNumSongs()]}.mp3`;
+  resImg.src = `./images/Friend/${friendImages[randomNum()]}.jpg`
 }
 function love() {
   mainpara.innerHTML = `IF YOU WANT TO FEEL WHAT THE ACTUAL <span class="side">LOVE❤</span> IS`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = loveMov[randomNumMov()];
-  audio.src = `./audio/${loveSongs[randomNum()]}.mp3`;
-  resImg.src = "./images/love1.jpg"
+  movieName.textContent = loveMov[randomNum()];
+  audio.src = `./audio/Love/${loveSongs[randomNumSongs()]}.mp3`;
+  resImg.src = `./images/Love/${loveImages[randomNum()]}.jpg`
 }
 function affection() {
   mainpara.innerHTML = `IT'S LIKE A <span class="side">ONE SIDE LOVE💔</span>, IF YOU WANT TO FEEL THAT`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = affectMov[randomNumMov()];
-  audio.src = `./audio/${affSongs[0]}.mp3`;
-  resImg.src = "./images/affect1.jpg"
+  movieName.textContent = affectMov[randomNum()];
+  audio.src = `./audio/Affection/${affSongs[randomNumSongs()]}.mp3`;
+  resImg.src = `./images/Affection/${affectImages[randomNum()]}.jpg`
   
 }
 function marriage() {
   mainpara.innerHTML = `IF YOU WANT TO FEEL WHAT THE ACTUAL <span class="side">MARRIAGE💑</span> IS`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = marriageMov[randomNumMov()];
-  audio.src = `./audio/${marrSongs[randomNum()]}.mp3`;
-  resImg.src = "./images/marriage.jpg"
+  movieName.textContent = marriageMov[randomNum()];
+  audio.src = `./audio/Marriage/${marrSongs[randomNumSongs()]}.mp3`;
+  resImg.src = `./images/Marriage/${marriageImages[randomNum()]}.jpg`
 }
 function enemy() {
   mainpara.innerHTML = `IF YOU WANT TO KNOW WHAT THE <span class="side">ENEMY😡</span> IS`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = enemyMov[randomNumMov()];
+  movieName.textContent = enemyMov[randomNum()];
   audio.src = `./audio/${enemSongs[0]}.mp3`;
-  resImg.src = "./images/enemy1.jpg"
+  resImg.src = `./images/Enemy/${enemyImages[randomNum()]}.jpg`
 }
 function sister() {
   mainpara.innerHTML = `IF YOU WANT TO FEEL WHAT THE ACTUAL <span class="side">SISTER LOVE❤</span> IS`
   let side = document.querySelector('.side').style = "color:red;";
-  movieName.textContent = sisterMov[randomNumMov()];
-  audio.src = `./audio/${sisSongs[randomNum()]}.mp3`;
-  resImg.src = "./images/sister.jpg"
+  movieName.textContent = sisterMov[randomNum()];
+  audio.src = `./audio/Sister/${sisSongs[randomNumSongs()]}.mp3`;
+  resImg.src = `./images/Sister/${sisterImages[randomNum()]}.jpg`
 }
-
-
 
 function randomNum() {
-  random = Math.floor(Math.random() * 2);
+  random = Math.floor(Math.random() * 5);
+  return random;
+}
+console.log(randomNum());
+function randomNumSongs() {
+  random = Math.floor(Math.random() * 5);
   return random;
 }
 
-function randomNumMov() {
-  random = Math.floor(Math.random() * 5);
-  return random;
-} 
+
+
